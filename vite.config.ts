09b98750fa63d3ec446d90f5aa96748fe16a2a68
agenciaazas <contato@agenciaazas.com>
2026-06-24 19:56:06 -0300

    fix pwa icons

--- vite.config.ts
+++ vite.config.ts
@@ -31,24 +31,19 @@ export default defineConfig({
   start_url: "/",
   scope: "/",
 
-  icons: [
-    {
-      src: "/pwa-192x192.png",
-      sizes: "192x192",
-      type: "image/png"
-    },
-    {
-      src: "/pwa-512x512.png",
-      sizes: "512x512",
-      type: "image/png"
-    },
-    {
-      src: "/pwa-512x512.png",
-      sizes: "512x512",
-      type: "image/png",
-      purpose: "maskable"
-    }
-  ]
+ icons: [
+  {
+    src: "/icon-192.png",
+    sizes: "192x192",
+    type: "image/png",
+  },
+  {
+    src: "/icon-512.png",
+    sizes: "512x512",
+    type: "image/png",
+  },
+]
+
 }
       }),
     ],
